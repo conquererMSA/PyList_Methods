668f0239290604ba1c5c784ebb37135e2af049c0
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-24 15:03:27 +0600

    Access a single item of a list.

--- listIntro.py
+++ listIntro.py
@@ -8,3 +8,13 @@ variable.
 list=['name', 30, 45,3+j,true] diff data type
 array=[20.30,56] simillar data type
 '''
+
+#list indexing
+'''
+list er single item access korar jonno indexing use kora hoy.
+list1=[23,6+j7,'yes']
+list er protity item er jonno duiti index ache:
+1. positive indexing //firstItem=list1[0] =>23
+2. negative indexing //firstItem=list[-3] =>23
+and list er multiple item access korar jonno slicing use kora hoy.
+'''
